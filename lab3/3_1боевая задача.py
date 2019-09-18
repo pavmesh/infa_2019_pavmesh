7245efec246cf41ebdea1ce4817a8177be076f11
Pavel Meshcheryakov <petya.123007@yandex.ru>
2019-09-18 11:55:51 +0300

    рисунок муж и жена 3_1

---
+++ lab3/3_1боевая задача.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 10:34:14 2019
+
+@author: ddd
+"""
+from graph import *
+brushColor(0,200,255)
+rectangle(0, 0, 600, 350)
+brushColor(0,200,25)
+
+rectangle(0, 700, 600, 350)
+
+brushColor(220,0,130)
+polygon([(250,400), (300,150),(350,400), (250,400)])
+
+polygon([(270,400),(270,490)])
+polygon([(270,490),(240,490)])
+polygon([(330,400),(330,490)])
+polygon([(330,490),(360,490)])
+
+penColor(155,155,155)
+brushColor(155,155,155)
+circle(175,190,30)
+circle(175,220,35)
+circle(175,250,40)
+circle(175,280,45)
+circle(175,310,40)
+circle(175,340,35)
+circle(175,370,30)
+
+penColor(0,0,0)
+
+brushColor(220,150,130)
+circle(300,150,40)
+circle(175,150,40)
+
+polygon([(230,280),(290,230)])
+polygon([(230,280),(190,230)])
+polygon([(360,280),(310,230)])
+polygon([(360,280),(380,160)])
+brushColor(255,0,0)
+penColor(255,0,0)
+
+polygon([(380,160),(370,130),(390,130)])
+circle(375,130,6)
+circle(385,130,6)
+
+penColor(0,0,0)
+
+polygon([(165,390),(135,490)])
+polygon([(135,490),(105,490)])
+polygon([(190,390),(190,490)])
+polygon([(190,490),(220,490)])
+
+polygon([(150,230),(110,300)])
+brushColor("yellow")
+polygon([(110,300),(120,230),(90,240)])
+
+brushColor("red")
+circle(115,230,10)
+
+
+brushColor("white")
+circle(100,235,10)
+
+
+brushColor("orange")
+circle(90,235,10)
+
+
+
+
+run()
